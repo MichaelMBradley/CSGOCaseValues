fa933910ea19b3870f6d3297f96d166dbb0fe134
Michael Bradley <michaelmbrad@gmail.com>
2021-04-30 20:31:01 -0400

    Update _main.py

--- _main.py
+++ _main.py
@@ -3,12 +3,10 @@ from vis import *
 
 
 def main():
-    # saveinfo()
-    # plotcases(readhistoricaldata())
+    saveinfo()
+    plotcases(readhistoricaldata())
     # printhistoricaldata(mostrecent=True, value=lambda case: case.EV)
     # dropped(recentcases())
-    cases, skins, skinfo, prices, caseCost = readinfo()
-    plotRelativePrices(prices, skinfo, skins)
 
 
 if __name__ == "__main__":
